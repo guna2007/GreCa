8055379d1d72ead80502bcf403675dda03a2335f
guna2007 <lankalapalligunapersonal@gmail.com>
2026-05-18 21:04:42 +0530

    UI oriented changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -32,8 +32,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-        Copyright {new Date().getFullYear()} © urssanjaysingh All Right
-        Reserved.
+        Copyright {new Date().getFullYear()} © L.Guna All Right Reserved.
       </p>
     </div>
   );
